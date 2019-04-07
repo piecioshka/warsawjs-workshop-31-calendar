3e1b673a98041ef86e477b1f9257ba59c29cac63
Piotr Kowalski <piecioshka@gmail.com>
2019-04-07 12:33:51 +0200

    Cleanup first test

--- test/base.router.test.js
+++ test/base.router.test.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const supertest = require('supertest');
-const bodyParser = require('body-parser');
 
 const router = require('../web/routing/base.router');
 
-let app = null;
-
-beforeEach(() => {
-    app = express();
-    app.use(bodyParser.json());
-    router(app);
-});
-
 it('should response on /', () => {
+    let app = express();
+    router(app);
+
     return supertest(app)
         .get('/')
         .expect(200)
